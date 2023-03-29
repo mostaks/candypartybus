dec222877ff39bb20bda520ea2285179925889e1
yianni.moustakas <yianni.moustakas@zip.co>
2023-03-29 21:18:09 +1100

    Add meta description

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Desktop from '../components/desktop'
 import Mobile from '../components/mobile'
-import Footer from '../components/footer'
+import Meta from '../components/meta'
 import styles from '../styles/Home.module.css'
 import { useWindowSize } from '../hooks/useWindowSize'
 
@@ -10,13 +8,8 @@ export default function Home() {
   const { deviceSize } = useWindowSize();
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Candy Party Bus Hire</title>
-        <meta name="description" content="Based in Sydney. Servicing areas from North to South Coast and everywhere in between" />
-        <link rel="icon" href="img/favicon/favicon.ico" />
-      </Head>
+      <Meta />
       {deviceSize === 'desktop' ? <Desktop /> : <Mobile />}
-
     </div>
   )
 }
