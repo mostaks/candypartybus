84c0bb8cd38def4468ea398a3843eb526180b229
yianni.moustakas <yianni.moustakas@zip.co>
2023-03-30 16:28:35 +1100

    Add site manifest

--- components/meta/Meta.jsx
+++ components/meta/Meta.jsx
@@ -17,6 +17,10 @@ const Meta = () => {
       />
       <meta name="keywords" content="Party bus Limousine rental Luxury transportation Group transportation Bachelor/bachelorette party Wedding transportation Prom transportation Corporate event transportation Nightlife transportation City tours Entertainment transportation Birthday party transportation Event rental Chauffeur service Shuttle service" />
       <link rel="icon" href="img/favicon/favicon.ico" />
+      <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" />
+      <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" />
+      <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 };
