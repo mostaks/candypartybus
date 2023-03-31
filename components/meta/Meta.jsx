63d340a0af6da79892087b2895f68b4b16e1233a
yianni.moustakas <yianni.moustakas@zip.co>
2023-03-31 15:51:18 +1100

    Cut down description2

--- components/meta/Meta.jsx
+++ components/meta/Meta.jsx
@@ -7,7 +7,7 @@ const Meta = () => {
   return (
     <Head>
       <title>Candy Party Bus Hire</title>
-      <meta name="description" content="Candy Party Bus is the premier provider of transportation services throughout Sydney from Newcastle to Wollongong. Our company specializes in providing safe, reliable, and affordable transportation for all types of events, from weddings and corporate events to birthday parties and nights out on the town."
+      <meta name="description" content="Candy Party Bus is the premier transportation provider in Sydney. We specialise in providing fun and affordable transportation for all event types, from weddings to birthday parties to nights out on the town."
       />
       <meta name="keywords" content="Party bus Limousine rental Luxury transportation Group transportation Bachelor/bachelorette party Wedding transportation Prom transportation Corporate event transportation Nightlife transportation City tours Entertainment transportation Birthday party transportation Event rental Chauffeur service Shuttle service" />
       <link rel="icon" href="img/favicon/favicon.ico" />
